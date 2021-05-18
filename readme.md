8bc88ae64304412ebaacb63dad6c153150912467
wirsodev <wilson.wmarques@gmail.com>
2021-05-18 18:14:42 +0100

    up readme - text size ploting

--- readme.md
+++ readme.md
@@ -1,3 +1,16 @@
-<h1>DRS Reports program</h1>
+# Drs reports
+
+
+## To do's:
+- Get the name of fabrics (excel file)
+- Generate all ploting
+- Upload ploted images to cloud service
+- Complete design of html email in  Stripo
+- Generate HTML email
+- Create main program:
+    - Run program every mounth;
+    - ...
+
+
 
 
--- src/PLOTS/plots.py
+++ src/PLOTS/plots.py
@@ -8,20 +8,23 @@ data = DataCollector()
 def markets(data):
 
     data_markets = data.getMarkets()
-    print(data_markets)
     x = []
     y = []
     for keys in data_markets.keys():
         x.append(keys)
     for values in data_markets.values():
-        print(values)
         y.append(values)
     fig = px.bar(x=x, y=y)
 
   
 
     fig.update_traces(texttemplate=y, textposition='inside')
-    fig.update_layout(uniformtext_minsize=2, uniformtext_mode='hide')
+    fig.update_layout(uniformtext_minsize=2, uniformtext_mode='hide', font=dict(
+        family="Courier New, monospace",
+        size=18,
+        color="RebeccaPurple"
+    ))
+
     #ploting and save images
     
     fig.write_image('./PLOTS/img/markets.jpg', width=700, height=350, scale=6)
@@ -30,13 +33,33 @@ def markets(data):
 def categories(data):
 
     data_markets = data.getCategory()
-    print(data_markets)
     x = []
     y = []
     for keys in data_markets.keys():
         x.append(keys)
     for values in data_markets.values():
-        print(values)
+        y.append(values)
+    fig = px.bar(x=x, y=y)
+
+    fig.update_traces(texttemplate=y, textposition='inside')
+    fig.update_layout(uniformtext_minsize=2, uniformtext_mode='hide', font=dict(
+        family="Courier New, monospace",
+        size=6,
+        color="Black"
+    ))
+    #ploting and save images
+
+    fig.write_image('./PLOTS/img/categories.jpg', width=700, height=350, scale=6)
+
+
+def requesttype(data):
+
+    data_markets = data.getRequestTypes()
+    x = []
+    y = []
+    for keys in data_markets.keys():
+        x.append(keys)
+    for values in data_markets.values():
         y.append(values)
     fig = px.bar(x=x, y=y)
 
@@ -44,7 +67,7 @@ def categories(data):
     fig.update_layout(uniformtext_minsize=2, uniformtext_mode='hide')
     #ploting and save images
 
-    fig.write_image('./PLOTS/img/categories.jpg', width=900, height=650, scale=6)
+    fig.write_image('./PLOTS/img/request_type.jpg', width=700, height=350, scale=6)
 
 
 
@@ -53,4 +76,5 @@ def main(isglobal:bool = False):
     data = DataCollector(isglobal)
     markets(data)
     categories(data)
+    requesttype(data)
     
